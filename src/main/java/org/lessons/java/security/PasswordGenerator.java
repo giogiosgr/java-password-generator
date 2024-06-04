a1f345ea448c027831b8a435abb2f6756c6f86c3
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-04 22:56:51 +0200

    feat: generazione password

--- src/main/java/org/lessons/java/security/PasswordGenerator.java
+++ src/main/java/org/lessons/java/security/PasswordGenerator.java
@@ -1,10 +1,37 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserire nome utente: ");
+		String firstName = sc.next();
+		
+		System.out.println("Inserire cognome utente: ");
+		String lastName = sc.next();
+		
+		System.out.println("Inserire colore preferito: ");
+		String color = sc.next();
 
+		System.out.println("Inserire giorno di nascita: ");
+		int day = sc.nextInt();
+		
+		System.out.println("Inserire mese di nascita: ");
+		int month = sc.nextInt();
+		
+		System.out.println("Inserire anno di nascita: ");
+		int year = sc.nextInt();
+		
+        int sum = day + month + year;
+        
+        String password = firstName+lastName+color+sum;
+        
+        System.out.printf("La password risultante è: %s", password);
+		
 	}
 
 }
